13d51f4b8487185a5997ead9a6b127514eeae83a
jorgelopezd <jorge_lopezd@hotmail.com>
2023-11-11 12:12:28 -0300

    se suben cambios

--- src/views/Pokemones.jsx
+++ src/views/Pokemones.jsx
@@ -1,37 +1,36 @@
-// codigo3.js
-import { useNavigate } from 'react-router-dom'
-import { useState, useEffect } from 'react'
-import { Button } from 'react-bootstrap'
-import '../App.css'
+import { useNavigate } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { Button } from 'react-bootstrap';
+import '../App.css';
 
 const Pokemon = () => {
-  const [pokemon, setPokemon] = useState([])
-  const [consultapokemon, Setconsultapokemon] = useState('')
-  const navigate = useNavigate()
+  const [pokemon, setPokemon] = useState([]);
+  const [consultapokemon, setConsultapokemon] = useState('');
+  const navigate = useNavigate();
 
-  const url = 'https://pokeapi.co/api/v2/pokemon/?limit=30&offset=0'
+  const url = 'https://pokeapi.co/api/v2/pokemon/?limit=30&offset=0';
 
   const infopokemon = async () => {
     try {
-      const res = await fetch(url)
-      const { results } = await res.json()
-      Setconsultapokemon(results)
+      const res = await fetch(url);
+      const { results } = await res.json();
+      setPokemon(results);
     } catch (error) {
-      console.error('error al momento de buscar la info:', error)
+      console.error('error al momento de buscar la info:', error);
     }
-  }
+  };
 
   const handleVerPokemon = () => {
-    if (pokemonSeleccionado) {
-      navigate(`/pokemones/${consultapokemon}`)
+    if (consultapokemon) {
+      navigate(`/pokemon/${consultapokemon}`);
     } else {
-      alert('Favor selecionar un pokemon')
+      alert('Favor seleccionar un pokemon');
     }
-  }
+  };
 
   useEffect(() => {
-    infopokemon()
-  }, [])
+    infopokemon();
+  }, []);
 
   return (
     <>
@@ -39,9 +38,9 @@ const Pokemon = () => {
         <h1>Seleccione un Pokemon</h1>
         <div className='selection-select'>
           <select
-            value={pokemonSeleccionado}
+            value={consultapokemon}
             className='form-select text-center'
-            onChange={(e) => Setconsultapokemon(e.target.value)}
+            onChange={(e) => setConsultapokemon(e.target.value)}
           >
             <option className='option' value='disabled'>
               Pokemon
@@ -58,7 +57,7 @@ const Pokemon = () => {
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
 export default Pokemon;
